942b6b99e8809d4067c4a332b9805586c32fc366
Михайлуц Юрий Вячеславович <aracks@yandex.ru>
2022-07-24 10:47:25 +0300

    Создан пустой проект.

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+	kotlin("multiplatform") version "1.7.10"
+}
+
+group = "mikhaylutsyury"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+kotlin {
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = "1.8"
+		}
+		withJava()
+		testRuns["test"].executionTask.configure {
+			useJUnitPlatform()
+		}
+	}
+	js(BOTH) {
+		browser {
+			commonWebpackConfig {
+				cssSupport.enabled = true
+			}
+		}
+	}
+	sourceSets {
+		val commonMain by getting
+		val commonTest by getting {
+			dependencies {
+				implementation(kotlin("test"))
+			}
+		}
+		val jvmMain by getting
+		val jvmTest by getting
+		val jsMain by getting {
+			dependencies {
+				implementation(npm("bigdecimal", "0.6.1"))
+			}
+		}
+		val jsTest by getting
+	}
+}
