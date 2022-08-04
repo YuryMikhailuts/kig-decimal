d524e1697fe447f95e4a91200c2cc62442d1d464
Михайлуц Юрий Вячеславович <aracks@yandex.ru>
2022-08-04 13:01:34 +0300

    Добавлены функции расширения, реализующие некоторые популярные операторы для BigInteger и BigDecimal.

--- src/commonMain/kotlin/mikhaylutsyury/kigdecimal/ext/BigDecimalExt.kt
+++ src/commonMain/kotlin/mikhaylutsyury/kigdecimal/ext/BigDecimalExt.kt
@@ -3,6 +3,7 @@ package mikhaylutsyury.kigdecimal.ext
 import mikhaylutsyury.kigdecimal.BigDecimal
 import mikhaylutsyury.kigdecimal.BigInteger
 import mikhaylutsyury.kigdecimal.makeBigDecimal
+import mikhaylutsyury.kigdecimal.toBigDecimal
 
 fun BigDecimal(text: String): BigDecimal = makeBigDecimal(text)
 fun BigDecimal(value: Int): BigDecimal = makeBigDecimal(value)
@@ -12,3 +13,47 @@ fun BigDecimal(value: Double): BigDecimal = makeBigDecimal(value)
 fun BigDecimal(value: BigInteger): BigDecimal = makeBigDecimal(value)
 
 fun BigDecimal.copy() = BigDecimal("$this")
+
+fun BigDecimal.intValue() = "$this".toInt()
+fun BigDecimal.longValue() = "$this".toLong()
+fun BigDecimal.uIntValue() = "$this".toUInt()
+fun BigDecimal.uLongValue() = "$this".toULong()
+fun BigDecimal.floatValue() = "$this".toFloat()
+fun BigDecimal.doubleValue() = "$this".toDouble()
+
+operator fun BigDecimal.unaryMinus() = 0.toBigDecimal() - this
+operator fun BigDecimal.unaryPlus() = this
+operator fun BigDecimal.inc() = this + 1
+operator fun BigDecimal.dec() = this - 1
+operator fun BigDecimal.plus(other: BigDecimal) = add(other)
+operator fun BigDecimal.minus(other: BigDecimal) = subtract(other)
+operator fun BigDecimal.times(other: BigDecimal) = multiply(other)
+operator fun BigDecimal.div(other: BigDecimal) = divide(other)
+
+operator fun BigDecimal.plus(other: Int) = add(other.toBigDecimal())
+operator fun BigDecimal.minus(other: Int) = subtract(other.toBigDecimal())
+operator fun BigDecimal.times(other: Int) = multiply(other.toBigDecimal())
+operator fun BigDecimal.div(other: Int) = divide(other.toBigDecimal())
+operator fun BigDecimal.plus(other: Long) = add(other.toBigDecimal())
+operator fun BigDecimal.minus(other: Long) = subtract(other.toBigDecimal())
+operator fun BigDecimal.times(other: Long) = multiply(other.toBigDecimal())
+operator fun BigDecimal.div(other: Long) = divide(other.toBigDecimal())
+operator fun BigDecimal.plus(other: BigInteger) = add(other.toBigDecimal())
+operator fun BigDecimal.minus(other: BigInteger) = subtract(other.toBigDecimal())
+operator fun BigDecimal.times(other: BigInteger) = multiply(other.toBigDecimal())
+operator fun BigDecimal.div(other: BigInteger) = divide(other.toBigDecimal())
+operator fun BigDecimal.plus(other: Float) = add(other.toBigDecimal())
+operator fun BigDecimal.minus(other: Float) = subtract(other.toBigDecimal())
+operator fun BigDecimal.times(other: Float) = multiply(other.toBigDecimal())
+operator fun BigDecimal.div(other: Float) = divide(other.toBigDecimal())
+operator fun BigDecimal.plus(other: Double) = add(other.toBigDecimal())
+operator fun BigDecimal.minus(other: Double) = subtract(other.toBigDecimal())
+operator fun BigDecimal.times(other: Double) = multiply(other.toBigDecimal())
+operator fun BigDecimal.div(other: Double) = divide(other.toBigDecimal())
+
+/**
+ * Оператор сравнения.
+ * Несмотря на то, что, на первый взгляд, тут должна быть рекурсия, её тут нет.
+ * Но это никакая не магия. Просто функция-член класса [BigDecimal.compareTo] при вызове имеет приоритет перед функцией расширения.
+ */
+operator fun BigDecimal.compareTo(other: BigDecimal) = compareTo(other)
--- src/commonMain/kotlin/mikhaylutsyury/kigdecimal/ext/BigInteger.kt
+++ src/commonMain/kotlin/mikhaylutsyury/kigdecimal/ext/BigInteger.kt
@@ -2,7 +2,40 @@ package mikhaylutsyury.kigdecimal.ext
 
 import mikhaylutsyury.kigdecimal.BigInteger
 import mikhaylutsyury.kigdecimal.makeBigInteger
+import mikhaylutsyury.kigdecimal.toBigInteger
 
 fun BigInteger(text: String, radix: Int = 10): BigInteger = makeBigInteger(text, radix)
 fun BigInteger(value: Int): BigInteger = makeBigInteger(value)
 fun BigInteger(value: Long): BigInteger = makeBigInteger(value)
+
+fun BigInteger.copy() = BigInteger("$this")
+
+fun BigInteger.intValue() = "$this".toInt()
+fun BigInteger.longValue() = "$this".toLong()
+fun BigInteger.uIntValue() = "$this".toUInt()
+fun BigInteger.uLongValue() = "$this".toULong()
+
+operator fun BigInteger.unaryMinus() = 0.toBigInteger() - this
+operator fun BigInteger.unaryPlus() = this
+operator fun BigInteger.inc() = this + 1
+operator fun BigInteger.dec() = this - 1
+operator fun BigInteger.plus(other: BigInteger) = add(other)
+operator fun BigInteger.minus(other: BigInteger) = subtract(other)
+operator fun BigInteger.times(other: BigInteger) = multiply(other)
+operator fun BigInteger.div(other: BigInteger) = divide(other)
+
+operator fun BigInteger.plus(other: Int) = add(other.toBigInteger())
+operator fun BigInteger.minus(other: Int) = subtract(other.toBigInteger())
+operator fun BigInteger.times(other: Int) = multiply(other.toBigInteger())
+operator fun BigInteger.div(other: Int) = divide(other.toBigInteger())
+operator fun BigInteger.plus(other: Long) = add(other.toBigInteger())
+operator fun BigInteger.minus(other: Long) = subtract(other.toBigInteger())
+operator fun BigInteger.times(other: Long) = multiply(other.toBigInteger())
+operator fun BigInteger.div(other: Long) = divide(other.toBigInteger())
+
+/**
+ * Оператор сравнения.
+ * Несмотря на то, что, на первый взгляд, тут должна быть рекурсия, её тут нет.
+ * Но это никакая не магия. Просто функция-член класса [BigInteger.compareTo] при вызове имеет приоритет перед функцией расширения.
+ */
+operator fun BigInteger.compareTo(other: BigInteger) = compareTo(other)
